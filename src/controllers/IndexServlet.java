0349c02793136f0d49abc88746199ef859e968cb
soichiro.aoyagi <soichiro.aoyagi@growship.com>
2021-11-16 16:28:36 +0900

    更新

--- src/controllers/IndexServlet.java
+++ src/controllers/IndexServlet.java
@@ -45,14 +45,13 @@ public class IndexServlet extends HttpServlet {
                                    .setMaxResults(15)
                                    .getResultList();
 
-        // 全件数を取得
         long tasks_count = (long)em.createNamedQuery("getTasksCount", Long.class)
                                       .getSingleResult();
 
         em.close();
 
         request.setAttribute("tasks", tasks);
-        request.setAttribute("tasks_count", tasks_count);     // 全件数
+        request.setAttribute("tasks_count", tasks_count);   
         request.setAttribute("page", page);
 
 
