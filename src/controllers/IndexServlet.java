ccc763faae1489e06c8faa819fb6089568ab9824
soichiro.aoyagi <soichiro.aoyagi@growship.com>
2021-11-16 13:48:50 +0900

    CreateServletの作成

--- src/controllers/IndexServlet.java
+++ src/controllers/IndexServlet.java
@@ -1,7 +1,9 @@
-package controllors;
+package controllers;
 
 import java.io.IOException;
+import java.util.List;
 
+import javax.persistence.EntityManager;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,18 +12,19 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import models.Task;
+import utils.DBUtil;
 
 /**
- * Servlet implementation class NewServlet
+ * Servlet implementation class IndexServlet
  */
-@WebServlet("/new")
-public class NewServlet extends HttpServlet {
+@WebServlet("/index")
+public class IndexServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public NewServlet() {
+    public IndexServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -30,14 +33,16 @@ public class NewServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // CSRF対策
-        request.setAttribute("_token", request.getSession().getId());
+        EntityManager em = DBUtil.createEntityManager();
 
-        // おまじないとしてのインスタンスを生成
-        request.setAttribute("task", new Task());
+        List<Task> tasks = em.createNamedQuery("getAllTasks", Task.class).getResultList();
 
-        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/new.jsp");
+        em.close();
+
+        request.setAttribute("tasks", tasks);
+
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/index.jsp");
         rd.forward(request, response);
-     }
+    }
 
 }
